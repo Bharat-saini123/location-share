8caa9e64d0ac54b12dc53d2f42f98385fddd5321
sainibharat277@gmail.com <bharatgknext@gmail.com>
2026-04-09 17:02:03 +0530

    update changes

--- lib/pusher-client.ts
+++ lib/pusher-client.ts
@@ -1,12 +1,13 @@
 "use client";
 
 import PusherClient from "pusher-js";
-export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_KEY!,
-  {
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-  }
-);
+
+export const pusherClient =
+  typeof window !== "undefined"
+    ? new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+      })
+    : (null as unknown as PusherClient);
 
 export const CHANNEL = "friend-locations";
 export const EVENT_LOCATION_UPDATE = "location-update";
